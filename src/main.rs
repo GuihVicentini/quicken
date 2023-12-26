dccc26e586b9f99f2bacc1210b7ecba6747bd5fe
guihvicentini <guilherme.h.vicentini@hotmail.com>
2023-12-26 12:12:55 +0100

    added uuid generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+use uuid::Uuid;
+
+#[derive(Parser, Debug)]
+#[command(
+    author,
+    version,
+    about,
+    long_about = None
+)]
+struct Cli {
+    #[arg(index = 1, default_value = "uuid")]
+    generator: String,
+
+    #[arg(short, long, default_value_t = 1)]
+    amount: u32,
+}
+
+fn generate_uuid(amount: u32) {
+    for _n in 0..amount {
+        println!("{}", Uuid::new_v4());
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    generate_uuid(args.amount);
 }
